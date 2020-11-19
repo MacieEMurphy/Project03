81871303758c5404435e1117bffcea08afc3cad6
Macie M <mem18000(@utdallas.edu>
2020-11-19 17:02:46 -0600

    November 19th Update

--- Assets/Scripts/Charge.cs
+++ Assets/Scripts/Charge.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Charge : MonoBehaviour
 {
+    private bool m_Charge = false;
+    private float m_walkSpeed;
+    private float m_turnSpeed;
+    private float m_chargeSpeed = 10.0f;
+    private float m_chargeTurn = 3.0f; 
+    public KeyCode chargeKey = KeyCode.LeftShift;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,16 @@ public class Charge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(chargeKey))
+        {
+            m_Charge = !m_Charge;
+
+            CheckCharge();
+        }
+    }
+
+    void CheckCharge()
+    {
+
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSInput))]
+[RequireComponent(typeof(FPSMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    FPSInput _input = null;
+    FPSMotor _motor = null;
+    AudioSource audioSource = null;
+    
+    public float playerHealth = 5.0f;
+    public Slider healthBar;
+    public bool alive;
+
+    [SerializeField] float _moveSpeed = 0.1f;
+    [SerializeField] float _walkSpeed = 0.1f;
+    [SerializeField] float _turnSpeed = 6.0f;
+    [SerializeField] float _jumpStrength = 10.0f;
+
+
+    private void Start()
+    {
+        alive = true;
+    }
+
+
+    private void Awake()
+    {
+        _input = GetComponent<FPSInput>();
+        _motor = GetComponent<FPSMotor>();
+    }
+
+    private void OnEnable()
+    {
+        _input.MoveInput += OnMove;
+        _input.RotateInput += OnRotate;
+        _input.JumpInput += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        _input.MoveInput -= OnMove;
+        _input.RotateInput -= OnRotate;
+        _input.JumpInput -= OnJump;
+    }
+
+    void OnMove(Vector3 movement)
+    {
+        _motor.Move(movement * _moveSpeed);
+    }
+
+    void OnRotate(Vector3 rotation)
+    {
+        _motor.Turn(rotation.y * _turnSpeed);
+        _motor.Look(rotation.x * _turnSpeed);
+    }
+
+    void OnJump()
+    {
+        _motor.Jump(_jumpStrength);
+    }
+
+    void Update()
+    {
+
+        healthBar.value = playerHealth;
+
+    }
+}
